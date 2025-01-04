15a7caff1842c080635189989799d40144deb8c4
ankuxsshh <ankuxshh72@gmail.com>
2025-01-04 15:22:15 +0530

    first commit

--- royal_hospital_app/urls.py
+++ royal_hospital_app/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('managements',views.managements,name="managements"),
+    path('facilities',views.facilities,name="facilities"),
+    path('departments',views.departments, name="departments"),
+    path('contactus',views.contactus,name="contactus"),
+    path('gallery',views.gallery,name="gallery"),
+    path('services',views.services,name="services"),
+    path('findadoctor',views.findadoctor,name="findadoctor")
 ]
--- royal_hospital_app/views.py
+++ royal_hospital_app/views.py
@@ -4,3 +4,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def departments(request):
+    return render(request, 'departments.html')
+
+def contactus(request):
+    return render(request, 'contactus.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def findadoctor(request):
+    return render(request, 'findadoctor.html')
+
+def facilities(request):
+    return render(request, 'facilities.html')
+
+def managements(request):
+    return render(request, 'managements.html')
